f419f90aa02fe3018da0c4c9fcba4e961ce30659
Gonçalo Nunes <goncaloinunes2002@gmail.com>
2021-03-27 22:04:50 +0000

    Implemented commands and created new structs

--- proj1.c
+++ proj1.c
@@ -17,6 +17,12 @@
 #define TASK_ACTIVITY_MAX 20 /* Maximum value of characters in the Task's Activity string. */
 #define TASK_MAX 50 /* Maximum ammount of simultaneous Tasks. */
 
+#define STRING_MAX 200 /* Maximum ammount of characters in each command. */
+
+#define USERS_MAX 50 /* Maximum ammount of users */
+
+
+
 /* STRUCTURES */
 
 /* Represents a Task with an id, description, user, activity, expected duration and start time. */
@@ -29,15 +35,70 @@ typedef struct {
     unsigned int start_time;
 } Task;
 
-/* Array of Tasks. */
-Task Tasks[TASK_MAX];
-/* Number of Tasks stored in memory. */
-unsigned short count = 0;
+
+/* Represents the Kanban Board with the number of existing tasks, current time and an array of all tasks. */
+typedef struct {
+    unsigned int task_count;
+    unsigned int time;
+    /* ->>>>>> Array of struct users */
+    /* ->>>>>> Array of struct activities */
+    Task tasks[TASK_MAX];
+} Kanban;
+
+/* Inicialize the task count and time fields of the Kanban struct. */
+Kanban kanban = {0, 0};
+
 
 
 /* Checks the user's input and acts accordingly. */
 int main() {
-    /* char command; */
+    char string[STRING_MAX];
+    char c, command = '_';  
+    unsigned int i = 0;  
+
+    while (command != 'q') {
+        while ((c = getchar()) != '\n')
+            string[i] = c;
+
+        i = 0;
+        command = string[0];
+
+        switch (command)
+        {
+         case 'q':
+            return 0;
+
+        case 't':
+            
+            break;
+
+        case 'l':
+            
+            break;
+
+        case 'n':
+            
+            break;
+
+        case 'u':
+            
+            break;
+
+        case 'm':
+            
+            break;
+
+        case 'd':
+            
+            break;
+        case 'a':
+            
+            break;
+        default:
+            printf("Error: Invalid Command: %c\n", command);
+            break;
+        }
+    }
 
     return 0;
 }
