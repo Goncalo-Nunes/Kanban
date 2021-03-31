bd544f42bc39ce50c90f2b016025a59a04f5c736
Gonçalo Nunes <goncaloinunes2002@gmail.com>
2021-03-31 10:06:40 +0100

    Finished command l

--- proj1.c
+++ proj1.c
@@ -22,6 +22,7 @@
 #define DESCRIPTION_MAX_CHAR 51 /* Maximum value of characters in the Task's Description string. */
 #define USER_MAX_CHAR 21 /* Maximum value of characters in the Task's User string. */
 #define ACTIVITY_MAX_CHAR 21 /* Maximum value of characters in the Task's Activity string. */
+#define ID_MAX_CHAR 6 /* Maximum amount of character in each id. */
 #define TASK_MAX 50 /* Maximum ammount of simultaneous Tasks. */
 #define ACTIVITY_MAX 10 /* Maximum ammount of Activities. */
 #define USER_MAX 50 /* Maximum ammount of Users. */
@@ -29,7 +30,7 @@
 #define ARGUMENT_MAX_CHAR 75
 #define ARGUMENTS_MAX 3
 
-#define STRING_MAX 200 /* Maximum ammount of characters in each command. */
+#define STRING_MAX 30000 /* Maximum ammount of characters in each command. */
 
 #define TODO "TO DO\0" /* String that represents the TO DO Activity. */
 #define IN_PROGRESS "IN PROGRESS\0" /* String that represents the TO DO Activity. */
@@ -37,6 +38,7 @@
 #define TASK "task" /* TASK string */
 #define ERROR_TOO_MANY_TASKS "too many tasks" /* Error string for too many tasks. */
 #define ERROR_DUPLICATE_DESCRIPTION "duplicate description" /* Error string for duplicate description. */
+#define ERROR_NO_SUCH_TASK "no such task" /* Error string for no such task. */
 
 
 #define ERROR -1 /* Error Code. */
@@ -62,7 +64,7 @@ typedef struct {
     unsigned int count;
 } Tasks;
 
-/* Struct with an array of all activities in alphabetical order and the ammount of current activities. */
+/* Struct with an array of all activities and the ammount of current activities. */
 typedef struct {
     char activity[ACTIVITY_MAX][ACTIVITY_MAX_CHAR];
     unsigned short count;
@@ -80,6 +82,7 @@ typedef struct {
     Users users;
     Activities activities;
     Tasks tasks;
+    char arguments[ARGUMENTS_MAX][ARGUMENT_MAX_CHAR];
 } Kanban;
 
 /* Global variable of type Kanban that represents the kanban board. */
@@ -89,45 +92,53 @@ Kanban kanban;
 
 /* FUNCTIONS */
 
+/* Initializes the Kanban board with the initial values. */
 void initialize_board() {
     kanban.time = 0;
     kanban.activities.count = 3;
     kanban.users.count = 0;
     kanban.tasks.count = 0;
-    strcpy(kanban.activities.activity[0], DONE);
+    strcpy(kanban.activities.activity[0], TODO);
     strcpy(kanban.activities.activity[1], IN_PROGRESS);
-    strcpy(kanban.activities.activity[2], TODO);
+    strcpy(kanban.activities.activity[2], DONE);
 }
 
-/* Function that modifies the arguments string with the user's input and returns True if at least of argument. */
-int get_arguments(char string[], char arguments[ARGUMENTS_MAX][ARGUMENT_MAX_CHAR], short num_args) {
-    int j, args, count = - 1;
+int argument_exists(char string[]) {
+    unsigned int i;
+    for (i = 1; i < strlen(string)-1; i++) {
+        if (string[i] != ' ' || string[i] != '\n')
+             return TRUE;
+    }
+
+    return FALSE;
+}
+
+/* Function that modifies the kanban.arguments string with the user's input. */
+void get_arguments(char string[], short num_args) {
+    int j, args;
     unsigned int i;
     int flag = NO_CHAR_FOUND;
 
     j=0; args=0;
-    for(i=1;i<=(strlen(string));i++)
+    for(i=1;i<=ARGUMENT_MAX_CHAR;i++)
     {
         /* if space or NULL found, assign NULL into arguments[args] */
-        if((string[i]==' '|| string[i]=='\0') && args < (num_args - 1))
+        if((string[i] == ' '|| string[i] == '\0') && args < (num_args - 1))
         {
             if (flag == FOUND_CHAR) {
-                arguments[args][j]='\0';
+                kanban.arguments[args][j]='\0';
                 args++;  
-                count++;
                 j=0;
                 flag = NO_CHAR_FOUND;
             }
         }
         else
         {  
-            arguments[args][j]=string[i];
+            kanban.arguments[args][j]=string[i];
             j++;
             flag = FOUND_CHAR;
         }
     }
-
-    return args > 0;
 }
 
 
@@ -160,7 +171,7 @@ int add_task(int duration, char description[]) {
         return ERROR;
     }
 
-    if (duration < 0) { 
+    if (duration < 0 || strlen(description) > DESCRIPTION_MAX_CHAR) { 
         return ERROR;
     }
     
@@ -174,20 +185,65 @@ int add_task(int duration, char description[]) {
     printf("%s %d\n", TASK, kanban.tasks.task[kanban.tasks.count].id);
     kanban.tasks.count++;
     
-
     return kanban.tasks.task[kanban.tasks.count].id - 1;
 }
 
+/* Prints the task's details based on the given id.  */
+int list_task(unsigned int id) { 
+    int i = id - 1;
+
+    if (id > 0 && id <= kanban.tasks.count) {
+        printf("%d %s #%d %s\n", kanban.tasks.task[i].id, kanban.tasks.task[i].activity,\
+            kanban.tasks.task[i].expected_duration, kanban.tasks.task[i].description);
+        return TRUE;
+    }
+
+    printf("%s\n", ERROR_NO_SUCH_TASK);
+    return FALSE;
+}
 
 
 void handle_command_t(char string[]) {
     unsigned short num_args = 2;
-    char arguments[ARGUMENTS_MAX][ARGUMENT_MAX_CHAR];
  
-    get_arguments(string, arguments, num_args);
-    add_task(atoi(arguments[0]), arguments[1]);
+    get_arguments(string, num_args);
+    add_task(atoi(kanban.arguments[0]), kanban.arguments[1]);
 }
 
+
+void handle_command_l(char string[]) {
+    unsigned int i, j = 0;
+    char id[ID_MAX_CHAR];
+    short flag = NO_CHAR_FOUND;
+
+    if (argument_exists(string)) {
+        /* Find each id inserted */
+        for (i = 1; i <= strlen(string); i++) {
+            if (string[i] == ' ' || string[i] == '\0') {
+                if (flag == FOUND_CHAR) {
+                    id[j] = '\0';
+                    j = 0;
+                    flag = NO_CHAR_FOUND;
+                    list_task(atoi(id)); /* print the task's information */
+                }
+            } else {
+                id[j] = string[i];
+                j++;
+                flag = FOUND_CHAR;
+            }
+
+        }
+    } else {
+        /* List all tasks */
+        for (i = 1; i <= kanban.tasks.count; i++)
+            list_task(i);
+    }  
+    
+}
+
+void handle_command_n(char string[]) {
+    
+}
 
 /* Checks the user's input and acts accordingly. */
 int main() {
@@ -198,14 +254,14 @@ int main() {
     initialize_board();
 
     while (command != 'q') {
-        while ((c = getchar()) != '\n') {
+        while ((c = getchar()) != '\n' && i < STRING_MAX) {
             if (c == EOF)
                 return 0;
             string[i] = c;
             i++;
         }
 
-        string[i] = '\0'; /* Adiciona um byte nulo no fim da string */
+        string[i] = '\0'; /* Add null byte to the end of the string */
         i = 0;
         command = string[0];
 
@@ -219,11 +275,11 @@ int main() {
             break;
 
         case 'l':
-            
+            handle_command_l(string);
             break;
 
         case 'n':
-            
+            handle_command_n(string);
             break;
 
         case 'u':
